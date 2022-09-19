4bf9cf6867c603719c27c85fa086055fa5f0db3a
Chen Ling <chenling9727@gmail.com>
2022-09-20 02:15:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def get_words():
   words = ["뱉는 순간 heartbeat, bum, bum, bum! Cool kid 다운 kick on the drum, drum, drum!", "令人愉悦的声音 笑意盈盈；一同唱起的歌声 一起去往那时 愉快的曾经。", "Trouble, trouble like a miscode, 비상이 걸려 버-버퍼링이야, I’m on that Glitch Mode", "Scratch that bring it back! 네 앞에선", "以最高的声音呼唤你的名字, 也许你就会听到吧；花朵在最为悲痛的春天盛开的话, 我会觉得那就像你的声音。", "So let me remember 오늘 같은 밤이면", "我会随着那耀眼的星辰, 找到再次回来的路 It's Never Goodbye.", "那时你与我在星光上描绘的梦, 多美好 大家一起哭笑的记忆。", "耀眼的那时, 那天的我们；曾炙热的日子, 在夜空中点亮的梦。", "You are the most important person in your life. So be yourself. Be beautiful.",
             "无论漆黑夜空中闪亮的星辰, 亦或落日余晖时的太阳, 都是独一无二的存在。", "I'm doing me regardless and I don't care what you say about it.", "Cause you're my word. 그 어떤 말이 너만큼 많이 빛날까?", "My favorite song 모두 네가 있어。", "This time you can face the rain; Next time you can beat the pain.", "无缘无故地闷闷不乐时, 暂且忘记现实来玩吧! 我这就去接你。",
             "Everyday we get better To be good aenergy!", "Everyday getting harder Be the one synergy!", "Two baddies, two baddies, one Porsche!", "Laugh it up, laugh it up, 네 고민들은 구석에다 던져 놔", "晚安 我的月光 快来投入我的怀抱；躺在夜幕低垂的涟漪上 或许你并不知晓 你的光芒多么绚丽美好。", "Gold dust up, my sea 금색 가룰 뿌려 찬란히"]
-  return words[1]#random.choice(words)
+  return words[0]#random.choice(words)
 
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
