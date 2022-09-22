a7dc0e4142612d2435d40d4f643cd3fa3e80f753
Chen Ling <chenling9727@gmail.com>
2022-09-22 20:40:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -48,18 +48,15 @@ def get_words():
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
 
-def get_bless():
-  bless = ["醒醒！醒醒！古德猫宁！", "今天也给我好好吃饭！", "今天也会有惊喜嘛！", "所以今天也要开开心心！", "zxr昨天做梦了吗！", "叮！又是幸运的一天！", "今天也要健健康康喔！>ε<"]
-  return random.choice(bless)
-
 client = WeChatClient(app_id, app_secret)
-
 wm = WeChatMessage(client)
 wea, high_temp, city, low_temp, cur_date = get_weather()
 color = get_random_color()
 week_list = ["周一","周二","周三","周四","周五","周六","周日"]
 weekday = week_list[datetime.strptime(cur_date, "%Y-%m-%d").weekday()]
-data = {"weekday":{"value":weekday, "color":color},"city":{"value":city, "color":color},"weather":{"value":wea, "color":color},"high_temp":{"value":high_temp, "color":color},"low_temp":{"value":low_temp, "color":color},"date":{"value":cur_date, "color":color},"days_from_birth":{"value":get_count(cur_date), "color":color},"birthday_left":{"value":get_birthday(cur_date), "color":color}, "words":{"value":get_words(), "color":get_random_color()}, "bless":{"value":get_bless(), "color":get_random_color()}}
+bless_list = ["叮！这周也要加油吖！", "今天也要健健康康喔！>ε<", "今天也给我好好吃饭！", "今天也会有惊喜嘛！", "好耶！终于到周五！", "zxr昨天做梦了吗！", "醒醒！醒醒！古德猫宁！"]
+bless = bless_list[datetime.strptime(cur_date, "%Y-%m-%d").weekday()]
+data = {"weekday":{"value":weekday, "color":color},"city":{"value":city, "color":color},"weather":{"value":wea, "color":color},"high_temp":{"value":high_temp, "color":color},"low_temp":{"value":low_temp, "color":color},"date":{"value":cur_date, "color":color},"days_from_birth":{"value":get_count(cur_date), "color":color},"birthday_left":{"value":get_birthday(cur_date), "color":color}, "words":{"value":get_words(), "color":get_random_color()}, "bless":{"value":bless, "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
 res2 = wm.send_template(lc_id, template_id, data)
