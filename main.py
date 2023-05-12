b19f2cd2535c3ac474407caf98f63bd2b39c77f1
Chen Ling <chenling9727@gmail.com>
2023-05-12 17:03:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -82,20 +82,24 @@ if days_to_next_birth==0:
 else:
   bless = bless_list[tmp]
 
-data = {"weekday":{"value":weekday, "color":color},
-        "city":{"value":city, "color":color},
-        "weather":{"value":wea, "color":color},
-        "high_temp":{"value":high_temp, "color":color},
-        "low_temp":{"value":low_temp, "color":color},
-        "date":{"value":cur_date, "color":color},
-        "days_to_graduation":{"value":get_graduation(cur_date), "color":color},
-        "days_from_birth":{"value":get_count(cur_date), "color":color},
-        "birthday_left":{"value":days_to_next_birth, "color":color},
-        "words":{"value":get_words(), "color":get_random_color()},
-        "bless":{"value":bless, "color":get_random_color()}}
+# data = {"weekday":{"value":weekday, "color":color},
+#         "city":{"value":city, "color":color},
+#         "weather":{"value":wea, "color":color},
+#         "high_temp":{"value":high_temp, "color":color},
+#         "low_temp":{"value":low_temp, "color":color},
+#         "date":{"value":cur_date, "color":color},
+#         "days_to_graduation":{"value":get_graduation(cur_date), "color":color},
+#         "days_from_birth":{"value":get_count(cur_date), "color":color},
+#         "birthday_left":{"value":days_to_next_birth, "color":color},
+#         "words":{"value":get_words(), "color":get_random_color()},
+#         "bless":{"value":bless, "color":get_random_color()}}
+data = {
+  "words":{"value":get_words(), "color":get_random_color()},
+  "bless":{"value":bless, "color":get_random_color()}}
+}
 print(data["words"])
 print(data["bless"])
-print(get_random_color())
+print(color)
 res = wm.send_template(user_id, template_id, data)
 print(res)
 res2 = wm.send_template(lc_id, template_id, data)
